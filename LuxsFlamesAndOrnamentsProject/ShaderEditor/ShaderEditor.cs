8dad99d7a033c2e38a87760289e04f2e5a1b0f8f
Pedro Costa <pedrocosta@infinitygames.io>
2023-07-13 18:04:05 +0100

    More LFO Editor

---
+++ LuxsFlamesAndOrnamentsProject/ShaderEditor/ShaderEditor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace LuxsFlamesAndOrnaments.ShaderEditor
+{
+    public class ShaderEditor
+    {
+        public static AnimationCurve Flat_1 => new AnimationCurve(new Keyframe(0, 1), new Keyframe(1, 1));
+        public static AnimationCurve Flat_0 => new AnimationCurve(new Keyframe(0, 0), new Keyframe(1, 0));
+        public static AnimationCurve Linear_01 => new AnimationCurve(new Keyframe(0, 0), new Keyframe(1, 1));
+        public static AnimationCurve Linear_10 => new AnimationCurve(new Keyframe(0, 1), new Keyframe(1, 0));
+    }
+}

27f911f0be9c423248aa92fa4843d4584d2081a0
Pedro Costa <pedrocosta@infinitygames.io>
2023-07-13 18:04:56 +0100

    Added Spacewarps Loading Action for plumes

--- LuxsFlamesAndOrnamentsProject/LuxsFlamesAndOrnamentsPlugin.cs
+++ LuxsFlamesAndOrnamentsProject/LuxsFlamesAndOrnamentsPlugin.cs
@@ -10,6 +10,15 @@ using SpaceWarp.API.UI;
 using SpaceWarp.API.UI.Appbar;
 using UnityEngine;
 using System.IO;
+using LuxsFlamesAndOrnaments.Settings;
+using BepInEx.Logging;
+using KSP.Game;
+using UnityEngine.AddressableAssets;
+using Newtonsoft.Json;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using KSP.Game.Flow;
+using RTG;
+using System.Runtime.InteropServices;
 
 namespace LuxsFlamesAndOrnaments;
 
@@ -23,7 +32,27 @@ public class LuxsFlamesAndOrnamentsPlugin : BaseSpaceWarpPlugin
     public const string ModVer = MyPluginInfo.PLUGIN_VERSION;
     public static LuxsFlamesAndOrnamentsPlugin Instance { get; set; }
 
-    public List<LFOConfig> configs;
+    public static void Log(LogLevel Level, object Data) => Instance.Logger.Log(Level, Data);
+    public static void Log(object Data) => Instance.Logger.LogInfo(Data);
+    public static void LogDebug(object Data) => Instance.Logger.LogDebug(Data);
+    public static void LogWarning(object Data) => Instance.Logger.LogWarning(Data);
+    public static void LogError(object Data) => Instance.Logger.LogError(Data);
+
+    public static string Path;
+
+    public List<string> requestedShaders = new();
+    public List<string> requestedMeshes = new();//Add caching of meshes
+
+    void Awake()
+    {
+        SpaceWarp.API.Loading.Loading.AddAssetLoadingAction("plumes", "Loading LFO plumes", ImportPlumes, "json");
+    }
+
+    public override void OnPreInitialized()
+    {
+        base.OnPreInitialized();
+        Path = PluginFolderPath;
+    }
 
     public override void OnInitialized()
     {
@@ -33,45 +62,80 @@ public class LuxsFlamesAndOrnamentsPlugin : BaseSpaceWarpPlugin
 
         Harmony.CreateAndPatchAll(typeof(LuxsFlamesAndOrnamentsPlugin).Assembly);
 
-        configs = new List<LFOConfig>();
+        SpaceWarp.API.Loading.SaveLoad.AddFlowActionToCampaignLoadAfter(new LoadShadersFlowAction(requestedShaders), "Loading Colony Data");
+    }
 
+    private List<(string name, UnityEngine.Object asset)> ImportPlumes(string internalPath, string filename)
+    {
+        var jsonData = File.ReadAllText(filename);
+        List<(string name, UnityEngine.Object asset)> assets = new();
 
+        LFOConfig config = LFOConfig.Deserialize(jsonData);
 
-        var allConfigsFiles = Directory.GetParent(PluginFolderPath).GetFiles("*.lfo", SearchOption.AllDirectories);
-        Logger.LogDebug($"Started loading configs, found {allConfigsFiles.Length} instances");
-        List<string> failedConfigs = new();
-        foreach(var file in allConfigsFiles)
+        if (config.PlumeConfigs is null && config.partName is null)
+            throw new Exception($"Plume config found at {filename} is not valid!");
+
+        LFO.RegisterLFOConfig(config.partName, config);
+        assets.Add(new($"lfo/plumes/{internalPath}", new TextAsset(jsonData)));
+
+        foreach (List<PlumeConfig> plumeConfigs in config.PlumeConfigs.Values)
         {
-            using (StreamReader sr = file.OpenText())
+            foreach (PlumeConfig plumeConfig in plumeConfigs)
             {
-                try
+                string toLoad = plumeConfig.ShaderSettings.ShaderName;
+                if (!requestedShaders.Contains(toLoad))
                 {
-                    string rawJson = sr.ReadToEnd();
-                    sr.Close();
-
-                    configs.AddRange(LFOConfig.Deserialize(rawJson));
-                }
-                catch
-                {
-                    Logger.LogError($"Couldn't load config at {file.FullName}");
-                    failedConfigs.Add(file.FullName);
+                    requestedShaders.Add(toLoad);
                 }
             }
         }
-        Logger.LogDebug($"Ended loading with {configs.Count} configs. {((failedConfigs.Count == 0) ? string.Empty : "\n\tMissing " + string.Join(", ", failedConfigs))}");
+
+        return assets;
     }
 
-    public bool TryGetLFOConfig(string key, out LFOConfig config)
+    public class LoadShadersFlowAction : FlowAction
     {
-        config = default;
-        try
+        private List<string> keys;
+        public LoadShadersFlowAction(List<string> requestedShaders) : base("Loading LFO Shaders...")
         {
-            config = configs.First(a => a.hierarchyPath == key);
-            return true;
+            this.keys = requestedShaders;
         }
-        catch
+
+        public override void DoAction(Action resolve, Action<string> reject)
         {
-            return false;
+            for (int i = 0; i < keys.Count; i++)
+            {
+                string toLoad = keys[i];
+
+                try
+                {
+                    string path = LFO.RESOURCES_PATH + toLoad.ToLower() + ".prefab";
+                    GameObject gameObject = SpaceWarp.API.Assets.AssetManager.GetAsset<GameObject>(path);
+
+                    if (gameObject is null)
+                        throw new ArgumentNullException($"Couldn't find material at {toLoad}");
+                    if (!gameObject.TryGetComponent(out Renderer renderer))
+                        throw new ArgumentNullException($"Loaded object at {gameObject.name} doesn't have a renderer!");
+                    if (renderer.material is null)
+                        throw new ArgumentNullException($"Loaded object at {gameObject.name}'s renderer doesn't have a material!");
+                    if (renderer.material.shader is null)
+                        throw new ArgumentNullException($"Loaded object at {gameObject.name}'s material doesn't have a shader!");
+                    if (renderer.material.shader.name != toLoad)
+                        Debug.LogWarning($"Shader name '{renderer.material.shader.name}' is different from key '{toLoad}'!");
+
+                    LFO.Instance.LoadedShaders.Add(toLoad, renderer.material.shader);
+                }
+                catch (Exception e)
+                {
+                    Debug.LogException(e);
+                }
+            }
+
+            resolve();
+        }
+
+        private void StoreShader(Shader shader)
+        {
         }
     }
 }
